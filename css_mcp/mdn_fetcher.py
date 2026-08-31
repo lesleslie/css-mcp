40943aebac4c0a19a784596dbc892059046d2555
lesleslie <les@wedgwoodwebworks.com>
2026-08-31 06:40:33 -0700

    chore: bump version to 0.5.2

--- css_mcp/mdn_fetcher.py
+++ css_mcp/mdn_fetcher.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import re
 from typing import Any
 
-import httpx
+import httpx2 as httpx
 from oneiric.core.logging import get_logger
 from pydantic import BaseModel, Field
 
